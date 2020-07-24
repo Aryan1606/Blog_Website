c5a5289d57c0e9fd8e3a42620c8b6f9195b046aa
Aryan1606 <aryansrivastava1606@gmail.com>
2020-07-24 18:22:12 +0530

    Wazza

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost:27017/blog2', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async(req,res)=>{
+    const articles= await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index', {articles: articles})
+})
+// mongoose.connect('mongodb://localhost/blog', {
+//   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+// })
+
+
+// app.use(express.urlencoded({ extended: false }))
+// app.use(methodOverride('_method'))
+
+// app.get('/', async (req, res) => {
+//   const articles = await Article.find().sort({ createdAt: 'desc' })
+//   res.render('articles/index', { articles: articles })
+// })
+app.use('/articles', articleRouter)
+
+app.listen(5000)
